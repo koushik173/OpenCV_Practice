577ea24e9d83dd9880abe1a5112ea1b03310b81a
koushik173 <cse_2012020173@lus.ac.bd>
2023-10-20 23:46:09 +0600

    working on border

--- makingBorder.py
+++ makingBorder.py
@@ -1,2 +1,12 @@
 import cv2
 import numpy as np
+
+img = cv2.imread("ironman.jpg")
+img = cv2.resize(img,(900,600))
+
+#creating image border
+border =cv2.copyMakeBorder(img, 10,10,10,10,cv2.BORDER_CONSTANT,value = [255,0,125])
+
+cv2.imshow("border",border)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
